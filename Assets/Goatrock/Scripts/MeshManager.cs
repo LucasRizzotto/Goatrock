97210f9119382428467391514766c6c31a0cef51
Lucas Rizzotto <contact@lucasrizzotto.com>
2019-01-18 09:02:17 -0800

    MeshManager added + commands for starting and pausing mesh capture

---
+++ Assets/Goatrock/Scripts/MeshManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+namespace GoatRock
+{
+    public class MeshManager : MonoBehaviour
+    {
+        public KeyCode ToggleMeshingDebugKey;
+
+        [Space(10)]
+
+        public MLSpatialMapper SpatialMapper;
+
+        public delegate void StartMeshBuildingEvent();
+        public static event StartMeshBuildingEvent OnMeshBuildingStart;
+        public static void StartMeshBuilding()
+        {
+            if (OnMeshBuildingStart != null)
+            {
+                Debug.Log("Started building the mesh!");
+                OnMeshBuildingStart();
+            }
+        }
+
+        public delegate void StopMeshBuildingEvent();
+        public static event StopMeshBuildingEvent OnMeshBuildingStop;
+        public static void StopMeshBuilding()
+        {
+            if (OnMeshBuildingStop != null)
+            {
+                Debug.Log("Stopped building the mesh.");
+                OnMeshBuildingStop();
+            }
+        }
+
+
+        #region Unity APIs
+
+        private void Awake()
+        {
+            MeshManager.OnMeshBuildingStart += BuildMesh;
+            MeshManager.OnMeshBuildingStop += StopBuild;
+        }
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(ToggleMeshingDebugKey))
+            {
+                ToggleMeshing();
+            }
+        }
+
+        #endregion
+
+        #region Main Methods
+
+        public void ToggleMeshing()
+        {
+            if(SpatialMapper.enabled)
+            {
+                StopMeshBuilding();
+            }
+            else
+            {
+                StartMeshBuilding();
+            }
+        }
+
+        public void BuildMesh()
+        {
+            SpatialMapper.enabled = true;
+        }
+
+        public void StopBuild()
+        {
+            SpatialMapper.enabled = false;
+        }
+
+        #endregion
+    }
+}
