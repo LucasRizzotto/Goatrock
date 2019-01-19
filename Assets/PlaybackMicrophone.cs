2d5bd26fa1910025f816f16109729bc97e17e3be
Lucas Rizzotto <contact@lucasrizzotto.com>
2019-01-18 16:34:17 -0800

    Segmented recording for audio clips'

--- Assets/PlaybackMicrophone.cs
+++ Assets/PlaybackMicrophone.cs
@@ -7,16 +7,32 @@ public class PlaybackMicrophone : MonoBehaviour {
 
     public AudioSource ThisAudioSource;
     public string MicrophoneName;
+    [Space(10)]
+    public KeyCode ToggleRecordingDebug = KeyCode.R;
+    public AudioClip CreatedAudioClip;
+
+    [Space(10)]
+    public float SamplesOnRecordingStart;
+    public float SamplesOnRecordingEnd;
+
+    private bool IsRecording = false;
+
+    #region Unity API
 
     private void Start()
     {
         if(Microphone.devices.Length > 0)
         {
             MicrophoneName = Microphone.devices[0].ToString();
-            ThisAudioSource.clip = Microphone.Start(MicrophoneName, true, 10, AudioSettings.outputSampleRate);
+            ThisAudioSource.clip = Microphone.Start(MicrophoneName, true, 300, AudioSettings.outputSampleRate);
             ThisAudioSource.Play();
             Debug.Log("Getting Mic Input Bois. Playing? " + ThisAudioSource.isPlaying);
         }
     }
+    
+
+
+    #endregion
+
 
 }
---
+++ Assets/RecordClipsOnLoudness.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordClipsOnLoudness : MonoBehaviour {
+
+    public float MinBand4;
+    public float MinBand3;
+    [Space(5)]
+    public AudioClip CreatedAudioClip;
+    public KeyCode ToggleRecordingDebug = KeyCode.R;
+    public AudioSource ThisAudioSource;
+    public float MaxClipLength;
+    public string MicrophoneName;
+    [Space(10)]
+    public float SamplesOnRecordingStart;
+    public float SamplesOnRecordingEnd;
+
+    private bool IsRecording = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(ToggleRecordingDebug))
+        {
+            ToggleRecording();
+        }
+    }
+
+    #region Recording Methods
+
+
+    public void ToggleRecording()
+    {
+        if (IsRecording)
+        {
+            EndAudioClipCreation();
+        }
+        else
+        {
+            BeginCreatingAudioClip();
+        }
+    }
+
+    public void BeginCreatingAudioClip()
+    {
+        Debug.Log("Began recording");
+        SamplesOnRecordingStart = Microphone.GetPosition(MicrophoneName);
+        IsRecording = true;
+    }
+
+    public void EndAudioClipCreation()
+    {
+        SamplesOnRecordingEnd = Microphone.GetPosition(MicrophoneName);
+
+        Debug.Log("Recording sample size: " + ((int)SamplesOnRecordingEnd - (int)SamplesOnRecordingStart));
+        Debug.Log("Initial sample size: " + (ThisAudioSource.clip.samples));
+
+        float[] samples = new float[(int)SamplesOnRecordingEnd - (int)SamplesOnRecordingStart];
+        ThisAudioSource.clip.GetData(samples, (int)SamplesOnRecordingStart);
+
+        var newClip = AudioClip.Create("Name", samples.Length, 1, AudioSettings.outputSampleRate, false);
+        newClip.SetData(samples, 0);
+        CreatedAudioClip = newClip;
+
+        var newAudioSource = gameObject.AddComponent<AudioSource>();
+        newAudioSource.clip = newClip;
+        newAudioSource.playOnAwake = true;
+        newAudioSource.loop = true;
+        newAudioSource.Play();
+
+        IsRecording = false;
+    }
+
+    #endregion
+
+}
